2294439f368bebc112f9a4b733aad23015d2f7ae
Santiago Tarnoski <santiagotarnoski9@gmail.com>
2025-06-03 11:28:27 -0300

    Update Admin.jsx

--- supermercado-frontend/src/pages/Admin.jsx
+++ supermercado-frontend/src/pages/Admin.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { API_BASE_URL, getImageUrl } from '../config/api.js';
+import { API_BASE_URL } from "../services/api";
 import { 
   Plus, 
   Edit3, 
