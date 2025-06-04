496ef2ab138b951490efeb679b58ad14778fd47d
Santiago Tarnoski <santiagotarnoski9@gmail.com>
2025-06-04 13:04:02 -0300

    Update vite.config.js

--- supermercado-frontend/vite.config.js
+++ supermercado-frontend/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    }
   },
   build: {
     cssCodeSplit: false,
